0aa692098dca0f0b3c15beaae29c58db1f1fdcb3
feriprasetiyo <ferytio40@gmail.com>
2022-11-17 19:34:43 +0700

    selesai

--- routes/goods.js
+++ routes/goods.js
@@ -9,9 +9,37 @@ module.exports = function (db) {
   router.get('/', isLoggedIn, function (req, res, next) {
     db.query('SELECT * FROM goods', (err, data) => {
       if (err) return res.send(err)
+      console.log(data)
       res.render('goods/goodindex', { user: req.session.user, current: 'goods', goods: data.rows });
     })
   });
+
+  router.get('/datatable', async (req, res) => {
+    let params = []
+
+    if (req.query.search.value) {
+      params.push(`barcode ilike '%${req.query.search.value}%'`)
+    }
+
+    if (req.query.search.value) {
+      params.push(`name ilike '%${req.query.search.value}%'`)
+    }
+
+    const limit = req.query.length
+    const offset = req.query.start
+    const sortBy = req.query.columns[req.query.order[0].column].data
+    const sortMode = req.query.order[0].dir
+
+    const total = await db.query(`select count(*) as total from goods${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
+    const data = await db.query(`select * from goods${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+    const response = {
+      "draw": Number(req.query.draw),
+      "recordsTotal": total.rows[0].total,
+      "recordsFiltered": total.rows[0].total,
+      "data": data.rows
+    }
+    res.json(response)
+  })
 
   router.get('/add', isLoggedIn, function (req, res, next) {
     db.query('SELECT * FROM units', (err, data) => {
@@ -22,7 +50,7 @@ module.exports = function (db) {
   router.post('/add', async (req, res) => {
     try {
       const { barcode, name, stock, purchase, selling, unit } = req.body
-      console.log(req.body)
+
       let sampleFile;
       let uploadPath;
       if (!req.files || Object.keys(req.files).length === 0) {
@@ -31,10 +59,11 @@ module.exports = function (db) {
 
       // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
       sampleFile = req.files.sampleFile;
-      uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', `${Date.now()}-${sampleFile.name}`);
+      const imagefiles = `${Date.now()}-${sampleFile.name}`
+      uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagefiles);
+      sampleFile.mv(uploadPath)
 
-
-      const { rows: data } = await db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchase, selling, unit, uploadPath])
+      const { rows: data } = await db.query('INSERT INTO goods (barcode, name, stock, purchaseprice, sellingprice, unit, picture) VALUES ($1, $2, $3, $4, $5, $6, $7)', [barcode, name, stock, purchase, selling, unit, imagefiles])
       res.redirect('/goods')
     } catch (err) {
       res.send(err)
@@ -43,36 +72,49 @@ module.exports = function (db) {
 
   router.get('/edit/:barcode', async (req, res) => {
     try {
+      const { rows: resut } = await db.query('SELECT * FROM units')
       const { barcode } = req.params
 
       const { rows: data } = await db.query('SELECT * FROM goods WHERE barcode = $1', [barcode])
-      res.render('goods/goodedit', { item: data[0], user: req.session.user, current: 'goods' })
+      res.render('goods/goodedit', { item: data[0], user: req.session.user, current: 'goods', units: resut })
     } catch (err) {
       res.send(err)
     }
   })
 
-  router.post('/edit/:unit', async (req, res) => {
+  router.post('/edit/:barcode', async (req, res) => {
     try {
-      const { unit } = req.params
-      console.log(req.params)
-      const { name, note } = req.body
-      console.log(req.body)
-      await db.query('UPDATE units SET name=$1, note=$2 WHERE unit=$3', [name, note, unit])
+      const { barcode } = req.params
+      const { name, stock, purchase, selling, unit } = req.body
 
-      res.redirect('/units')
+      let sampleFile;
+      let uploadPath;
+
+      if (!req.files || Object.keys(req.files).length === 0) {
+        await db.query('UPDATE goods SET name=$1, stock=$2, purchaseprice=$3, sellingprice=$4, unit=$5 WHERE barcode=$6',
+          [name, stock, purchase, selling, unit, barcode])
+      } else {
+        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+        sampleFile = req.files.sampleFile;
+        const imagefiles = `${Date.now()}-${sampleFile.name}`
+        uploadPath = path.join(__dirname, '..', 'public', 'images', 'upload', imagefiles);
+        sampleFile.mv(uploadPath)
+
+        await db.query('UPDATE goods SET name=$1, stock=$2, purchaseprice=$3, sellingprice=$4, unit=$5, picture=$6 WHERE barcode=$7', [name, stock, purchase, selling, unit, imagefiles, barcode])
+      }
+      res.redirect('/goods')
     } catch (err) {
+      console.log(err)
       res.send(err)
     }
   })
 
   router.get('/delete/:barcode', async (req, res) => {
-    console.log('masuk')
     try {
       const { barcode } = req.params
 
-      const { rows: data } = await db.query('DELETE FROM goods WHERE barcode = $1', [barcode])
-      res.redirect('/units')
+      const { rows } = await db.query('DELETE FROM goods WHERE barcode = $1', [barcode])
+      res.redirect('/goods')
     } catch (err) {
       res.send(err)
     }
--- routes/utilitis.js
+++ routes/utilitis.js
@@ -13,6 +13,38 @@ module.exports = function (db) {
     })
   });
 
+  router.get('/datatable', async (req, res) => {
+    let params = []
+
+    if (req.query.search.value) {
+      params.push(`unit ilike '%${req.query.search.value}%'`)
+    }
+
+    if (req.query.search.value) {
+      params.push(`name ilike '%${req.query.search.value}%'`)
+    }
+
+    if (req.query.search.value) {
+      params.push(`note ilike '%${req.query.search.value}%'`)
+    }
+
+
+    const limit = req.query.length
+    const offset = req.query.start
+    const sortBy = req.query.columns[req.query.order[0].column].data
+    const sortMode = req.query.order[0].dir
+
+    const total = await db.query(`select count(*) as total from units${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
+    const data = await db.query(`select * from units${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+    const response = {
+      "draw": Number(req.query.draw),
+      "recordsTotal": total.rows[0].total,
+      "recordsFiltered": total.rows[0].total,
+      "data": data.rows
+    }
+    res.json(response)
+  })
+
   router.get('/add', isLoggedIn, function (req, res, next) {
     res.render('utilitis/add', { user: req.session.user, current: 'units' });
   })
@@ -42,9 +74,7 @@ module.exports = function (db) {
   router.post('/edit/:unit', async (req, res) => {
     try {
       const { unit } = req.params
-      console.log(req.params)
       const { name, note } = req.body
-      console.log(req.body)
       await db.query('UPDATE units SET name=$1, note=$2 WHERE unit=$3', [name, note, unit])
 
       res.redirect('/units')
@@ -54,7 +84,6 @@ module.exports = function (db) {
   })
 
   router.get('/delete/:unit_id', async (req, res) => {
-    console.log('masuk')
     try {
       const { unit_id } = req.params
 
