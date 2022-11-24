0e903d9104cee2846c4ac8f3b4d3fb83be11fb32
feriprasetiyo <ferytio40@gmail.com>
2022-11-24 14:22:52 +0700

    selesai

--- query.sql
+++ query.sql
@@ -1,45 +1,50 @@
-CREATE OR REPLACE FUNCTION update_purchases() RETURNS TRIGGER AS $set_purchases$
+
+-- PEMBELIAN KE SUPLAYER
     DECLARE
     stock_lama INTEGER;
     total_harga NUMERIC;
+	current_invoice TEXT;
     BEGIN
         IF (TG_OP = 'INSERT') THEN
             SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.itemcode;
-            UPDATE goods SET stok = stock_lama - NEW.quantity WHERE barcode = NEW.itemcode;
-
+            UPDATE goods SET stock = stock_lama + NEW.quantity WHERE barcode = NEW.itemcode;
+			current_invoice := NEW.invoice;
+			
         ELSIF (TG_OP = 'UPDATE') THEN
             SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.itemcode;
-            UPDATE goods SET stok = stock_lama + OLD.quantity - NEW.quantity WHERE barcode = NEW.itemcode;
+            UPDATE goods SET stock = stock_lama - OLD.quantity + NEW.quantity WHERE barcode = NEW.itemcode;
+			current_invoice := NEW.invoice;
+	
 
         ELSIF (TG_OP = 'DELETE') THEN
-            SELECT stock INTO stock_lama FROM goods WHERE barcode = NEW.itemcode;
-            UPDATE goods SET stok = stock_lama + NEW.quantity WHERE barcode = NEW.itemcode;
-
+            SELECT stock INTO stock_lama FROM goods WHERE barcode = OLD.itemcode;
+            UPDATE goods SET stock = stock_lama - OLD.quantity WHERE barcode = OLD.itemcode;
+			current_invoice := OLD.invoice;
         END IF;
-        SELECT sum(totalprice) INTO total_harga FROM purchaseitems WHERE invoice = NEW.invoice;
-        UPDATE purchases set totalsum = total_harga WHERE invoice = NEW.invoice;
+		
+        SELECT coalesce(sum(totalprice), 0) INTO total_harga FROM purchaseitems WHERE invoice = current_invoice;
+        UPDATE purchases set totalsum = total_harga WHERE invoice = current_invoice;
 
-        RETRUN NULL
+        RETURN NULL;
     END;
-$set_purchases$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_purchases
-AFTER INSERT OR UPDATE OR DELETE ON purchaseitems
-    FOR EACH ROW EXECUTE FUNCTION update_purchases();
 
 -- UPDATE HARGA
-CREATE OR REPLACE FUNCTION update_harga() RETURNS TRIGGER AS $set_total_harga$
     DECLARE
         price_item NUMERIC;
     BEGIN
         SELECT purchaseprice INTO price_item FROM goods WHERE barcode = NEW.itemcode;
-        NEW.purchaseprice := price_item
-        NEW.totalprice := NEW.quantity * NEW.price_item;
+        NEW.purchaseprice := price_item;
+        NEW.totalprice := NEW.quantity * price_item;
             RETURN NEW;
-        RETRUN NULL
     END;
-$set_total_harga$ LANGUAGE plpgsql;
 
-CREATE TRIGGER set_total_harga
-BEFORE INSERT OR UPDATE OR DELETE ON purchaseitems
-    FOR EACH ROW EXECUTE FUNCTION update_harga();
+-- invoice sesuai tanggal
+BEGIN
+   IF EXISTS(SELECT invoice FROM purchases WHERE invoice = 'INV-' || to_char(current_date, 'YYYYMMDD') || -1) THEN
+      return 'INV-' || to_char(current_date, 'YYYYMMDD') || - nextval('invoice_seq');
+   ELSE
+   	ALTER SEQUENCE invoice_seq RESTART WITH 1;
+      return 'INV-' || to_char(current_date, 'YYYYMMDD') || - nextval('invoice_seq');
+   END IF;
+END
