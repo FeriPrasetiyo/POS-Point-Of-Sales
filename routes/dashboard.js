fd48fd6e1e707f515bc3678642c59bc7bd680c33
feriprasetiyo <ferytio40@gmail.com>
2022-11-30 12:56:13 +0700

    selesai

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -14,55 +14,35 @@ module.exports = function (db) {
 
       res.render('dashboard/dashboard', { user: req.session.user, current: 'dashboard', purhcases, sales, customers });
     } catch (err) {
-
+      res.send(err)
     }
   });
 
   router.get('/revsource', isLoggedIn, async (req, res, next) => {
     try {
       const { rows: direct } = await db.query('SELECT COUNT(*) FROM sales WHERE customer = 1')
-      console.log(direct)
       const { rows: member } = await db.query('SELECT COUNT(*) FROM sales WHERE customer != 1')
-      console.log(member)
 
-      res.json({ member, direct })
-    } catch (error) {
-      console.log(error)
+
+      const { rows: totalpurch } = await db.query("SELECT sum(totalsum)FROM purchases WHERE time BETWEEN '20221125' AND '20221225'")
+      console.log(totalpurch)
+      const { rows: totalsales } = await db.query("SELECT sum(totalsum)FROM sales WHERE time BETWEEN '20221125' AND '20221225'")
+      res.json({ member, direct, totalpurch, totalsales })
+    } catch (err) {
+      res.send(err)
     }
   })
 
 
-  router.get('/datatable', async (req, res) => {
-    let params = []
+  // router.get('/datatable', isLoggedIn, async (req, res, next) => {
+  //   try {
+  //     const { rows: data } = await db.query('SELECT time FROM sales CROSS JOIN purchases')
+  //     console.log(data, "<<")
 
-    if (req.query.search.value) {
-      params.push(`unit ilike '%${req.query.search.value}%'`)
-    }
-
-    if (req.query.search.value) {
-      params.push(`name ilike '%${req.query.search.value}%'`)
-    }
-
-    if (req.query.search.value) {
-      params.push(`note ilike '%${req.query.search.value}%'`)
-    }
-
-
-    const limit = req.query.length
-    const offset = req.query.start
-    const sortBy = req.query.columns[req.query.order[0].column].data
-    const sortMode = req.query.order[0].dir
-
-    const total = await db.query(`select count(*) as total from units${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
-    const data = await db.query(`select * from units${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
-    const response = {
-      "draw": Number(req.query.draw),
-      "recordsTotal": total.rows[0].total,
-      "recordsFiltered": total.rows[0].total,
-      "data": data.rows
-    }
-    res.json(response)
-  })
+  //   } catch (err) {
+  //     res.send(err)
+  //   }
+  // })
 
   return router;
 }
