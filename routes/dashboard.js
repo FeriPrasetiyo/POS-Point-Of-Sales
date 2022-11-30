665979d66b8a6812ba5a22809974379883e23262
feriprasetiyo <ferytio40@gmail.com>
2022-12-01 03:42:44 +0700

    selesai

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -32,6 +32,29 @@ module.exports = function (db) {
     }
   })
 
+  router.get('/datatable', async (req, res) => {
+    console.log("masuk")
+    let params = []
+
+    if (req.query.search.value) {
+      params.push(`barcode ilike '%${req.query.search.value}%'`)
+    }
+
+    const limit = req.query.length
+    const offset = req.query.start
+    const sortBy = req.query.columns[req.query.order[0].column].data
+    const sortMode = req.query.order[0].dir
+
+    const total = await db.query(`select count(*) as total from goods${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
+    const data = await db.query(`select * from goods${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+    const response = {
+      "draw": Number(req.query.draw),
+      "recordsTotal": total.rows[0].total,
+      "recordsFiltered": total.rows[0].total,
+      "data": data.rows
+    }
+    res.json(response)
+  })
   return router;
 }
 
